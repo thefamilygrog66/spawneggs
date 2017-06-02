01a829668a5c8c7ffae4d097194ec7ff7ca33ee9
AntumDeluge <antumdeluge@gmail.com>
2017-06-01 18:09:27 -0700

    Add option to disable egg node spawning:
    - spawneggs.enable_node_spawn

--- init.lua
+++ init.lua
@@ -3,6 +3,12 @@ local egg_chance = tonumber(minetest.settings:get('spawneggs.egg_chance')) or 30
 local grass_interval = tonumber(minetest.settings:get('spawneggs.grass_interval')) or 600
 local grass_chance = tonumber(minetest.settings:get('spawneggs.grass_chance')) or 3000
 
+-- Allow spawnegg nodes to spawn in world
+local enable_node_spawn = minetest.settings:get_bool('spawneggs.enable_node_spawn')
+if enable_node_spawn == nil then
+	enable_node_spawn = true
+end
+
 local spawneggs_list = {
 	{ "Spawn Dirt Monster", "dirt_monster", "default:dirt"},
 	{ "Spawn Dungeon Master", "dungeon_master", "default:mese"},	
@@ -71,15 +77,19 @@ minetest.register_craft({
     cooktime = 5,
 })
 
+
 -- Egg Spawning and De-spawning
-minetest.register_abm(
-	{nodenames = {"default:grass_1"},
-	interval = grass_interval,
-	chance = grass_chance,
-	action = function(pos)
-	minetest.env:add_node(pos, {name="spawneggs:egg"})
-	end,
-})
+
+if enable_node_spawn then
+	minetest.register_abm(
+		{nodenames = {"default:grass_1"},
+		interval = grass_interval,
+		chance = grass_chance,
+		action = function(pos)
+		minetest.env:add_node(pos, {name="spawneggs:egg"})
+		end,
+	})
+end
 
 minetest.register_abm(
 	{nodenames = {"spawneggs:egg"},
